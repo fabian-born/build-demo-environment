d40fd579e61364ba0268d80860aa66c99f0340e9
fabian-born <fabian.born@fabianborn.net>
2022-03-17 16:20:41 +0000

    y

--- k8s-in-gcp/gcp.tf
+++ k8s-in-gcp/gcp.tf
@@ -103,26 +103,3 @@ module "gke" {
   }
 }
 
-module "gke_auth" {
-  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  project_id   = var.gcp-project
-  location     = module.gke.location
-  cluster_name = module.gke.name
-}
-
-resource "local_file" "kubeconfig" {
-  content  = module.gke_auth.kubeconfig_raw
-  filename = "./gke-kubeconfig"
-}
-
-
-resource "null_resource" "kubectl" {
-  provisioner "local-exec" {
-    command = "kubectl ${var.cmd_snapshotter} --kubeconfig <(echo $KUBECONFIG | base64 --decode)"
-    interpreter = ["/bin/bash", "-c"]
-    environment = {
-      KUBECONFIG = base64encode(module.gke_auth.kubeconfig_raw)
-    }
-  }
-}
-
