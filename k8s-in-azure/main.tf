1cb22dd7e28e3241dfe5b0f82a04b84e7d0a4e90
Fabian Born <36699674+fabian-born@users.noreply.github.com>
2022-06-27 17:54:08 +0200

    Update main.tf

--- k8s-in-azure/main.tf
+++ k8s-in-azure/main.tf
@@ -1,14 +1,5 @@
 resource "random_pet" "prefix" {}
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
   /*tenant_id       = var.az_tenantId */
